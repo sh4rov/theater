4f122f625aa3e41fb8b891b818f4edf8dc450ace
Anatoly Sahrov <sh4rov@gmail.com>
2020-10-08 13:56:19 +0300

    full add

--- gulpfile.babel.js/index.js
+++ gulpfile.babel.js/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 import { series, parallel } from 'gulp';
-import html from './tasks/html';
+import pages from './tasks/html';
 import styles from './tasks/styles';
 import clean from './tasks/clean';
 import serve from './tasks/serve';
@@ -9,15 +9,17 @@ import fonts from './tasks/fonts';
 import images from './tasks/images';
 import js from './tasks/scripts';
 import svg from './tasks/svg';
+import data from './tasks/data';
 import cacheClear from './tasks/cacheClear';
 import grid from './tasks/smartGrid';
 
 global.paths = require('./paths');
 
-export const build = series(svg, parallel(html, styles, js, images, fonts));
+export const html = series(data, pages);
+export const build = series(clean, svg, parallel(data, html, styles, js, images, fonts));
 export const prod = series(clean, parallel(html, styles, js, images, fonts));
 
 
 export default serve;
 
-export { html, styles, images, fonts, js, serve, clean, cacheClear, grid, svg };
+export { pages, styles, images, fonts, js, serve, clean, data, cacheClear, grid, svg };
--- gulpfile.babel.js/paths.js
+++ gulpfile.babel.js/paths.js
@@ -11,6 +11,13 @@ module.exports = {
     watch: ['./src/pug/**/*.pug', './src/modules/**/*.pug'],
   },
 
+  data: {
+    src: './src/data/**/*.json',
+    dest: './src/data/temp',
+
+    watch: './src/data/**/*.json' + '' + '!data.json'
+  },
+
   styles: {
     src: './src/styles/**/*.scss',
     dest: './build/css',
