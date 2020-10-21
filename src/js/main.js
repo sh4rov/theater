e70fc21079926b305e46eb5fe89b5e88850ee6d9
kek <sh4rov@gmail.com>
2020-10-21 17:04:01 +0300

    commit

--- src/js/main.js
+++ src/js/main.js
@@ -10,3 +10,21 @@ const heroSwiper = new Swiper('.hero__swiper-container', {
     prevEl: '.hero__nav-btn_prev',
   },
 });
+
+// Open and close mobil-menu
+
+const btn_open = document.querySelector('.header__btn-menu');
+const btn_close = document.querySelector('.mobile-menu__btn-close');
+const mobile_menu = document.querySelector('.mobile-menu');
+
+function toggle_menu () {
+  btn_open.addEventListener('click', () => {
+    mobile_menu.classList.add('mobile-menu_open')
+  })
+  
+  btn_close.addEventListener('click', () => {
+    mobile_menu.classList.remove('mobile-menu_open')
+  })
+}
+
+toggle_menu();
