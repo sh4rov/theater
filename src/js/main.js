ff92d9949d8146bb7ea8dba9f73e863008e8bee7
kek <sh4rov@gmail.com>
2020-10-24 07:26:39 +0300

    adds

--- src/js/main.js
+++ src/js/main.js
@@ -1,30 +1,42 @@
-const heroSwiper = new Swiper('.hero__swiper-container', {
-  spaceBetween: 20,
-  speed: 400,
-  grabCursor: true,
-  slidesPerView: 1,
+(() => {
+  const heroSwiper = new Swiper('.hero__swiper-container', {
+    spaceBetween: 20,
+    speed: 400,
+    grabCursor: true,
+    slidesPerView: 1,
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.hero__nav-btn_next',
-    prevEl: '.hero__nav-btn_prev',
-  },
-});
+    // Navigation arrows
+    navigation: {
+      nextEl: '.hero__nav-btn_next',
+      prevEl: '.hero__nav-btn_prev',
+    },
+  });
 
-// Open and close mobil-menu
+  // Open and close mobil-menu
 
-const btn_open = document.querySelector('.header__btn-menu');
-const btn_close = document.querySelector('.mobile-menu__btn-close');
-const mobile_menu = document.querySelector('.mobile-menu');
+  const btnOpen = document.querySelector('.header__btn-menu');
+  const btnClose = document.querySelector('.mobile-menu__btn-close');
+  const mobileMenu = document.querySelector('.mobile-menu');
+  const classMenuOpen = 'mobile-menu_open';
+  const mql = window.matchMedia("(max-width: 768px)");
 
-function toggle_menu () {
-  btn_open.addEventListener('click', () => {
-    mobile_menu.classList.add('mobile-menu_open')
+  function removeClassMenuOpen() {
+    mobileMenu.classList.remove(classMenuOpen)
+  }
+
+  btnOpen.addEventListener('click', () => {
+    mobileMenu.classList.add(classMenuOpen)
   })
+  btnClose.addEventListener('click', removeClassMenuOpen)
+
   
-  btn_close.addEventListener('click', () => {
-    mobile_menu.classList.remove('mobile-menu_open')
-  })
-}
+  function setupForWidth(e) {
+    if (e.matches) {
+      removeClassMenuOpen;
+    }
+  }
 
-toggle_menu();
+  mql.addEventListener(setupForWidth);
+
+
+})()
